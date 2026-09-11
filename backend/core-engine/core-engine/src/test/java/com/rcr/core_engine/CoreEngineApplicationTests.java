a8f8407f2fa96ec6c3d40c1bc810c0d24df4a966
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-09-11 04:32:21 +0000

    feat: set up initial project structure with Maven and Spring Boot

---
+++ backend/core-engine/core-engine/src/test/java/com/rcr/core_engine/CoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcr.core_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
