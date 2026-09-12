dc7c30f0bfd565240202ee884cf4595fedda6323
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-09-12 14:40:37 +0000

    feat: implement CandidateAuthService for candidate registration

---
+++ backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/serviceImpl/CandidateAuthServiceImpl.java
@@ -0,0 +1,58 @@
+package com.rcr.core_engine.serviceImpl;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.rcr.core_engine.dtos.ApiResponse;
+import com.rcr.core_engine.dtos.CandidateRegistrationRequest;
+import com.rcr.core_engine.entity.User;
+import com.rcr.core_engine.repositories.UserRepository;
+import com.rcr.core_engine.services.CandidateAuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service 
+@RequiredArgsConstructor 
+public class CandidateAuthServiceImpl implements CandidateAuthService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+   
+    @Override 
+    public ApiResponse registerCandidate(CandidateRegistrationRequest request){
+        if(!isValidCaptcha(request.getCaptchaToken())){
+            throw new IllegalArgumentException("Invalid captcha Verification");
+        }
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email already exists!");
+        }
+
+        if(userRepository.existsByMobile(request.getMobile())){
+            throw new IllegalArgumentException("Same mobile number already exists!");
+        }
+
+        User candidate = User.builder()
+        .firstName(request.getFirstName().trim())
+        .lastName(request.getLastName().trim())
+        .mobile(request.getMobile().trim())
+        .password(passwordEncoder.encode(request.getMobile().trim()))
+        .dob(request.getDob())
+        .build();
+
+        userRepository.save(candidate);
+
+        ApiResponse response = new ApiResponse(true, "candidate registered successfully");
+
+        return response;
+    }
+
+    private Boolean isValidCaptcha(String captcha){
+       if(captcha != null){
+        return true;
+       }
+
+       return false;
+    }     
+}
