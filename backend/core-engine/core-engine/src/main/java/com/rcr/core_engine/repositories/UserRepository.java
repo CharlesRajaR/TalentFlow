0b783d3173ade207daac6e3b1bd94f47ddfb4ada
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-09-14 18:08:38 +0000

    feat: add findByMobile method to UserRepository for mobile-based user retrieval

--- backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/repositories/UserRepository.java
+++ backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/repositories/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByMobile(String mobile);
     Boolean existsByEmail(String email);
     Boolean existsByMobile(String mobile);
 
