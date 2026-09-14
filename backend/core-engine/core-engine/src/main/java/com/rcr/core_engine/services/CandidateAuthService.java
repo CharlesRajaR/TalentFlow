ad5d180c98fc5269483e07d28012b2820f5201d8
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-09-14 18:09:25 +0000

    feat: add sendOtp and login methods to CandidateAuthService interface

--- backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/services/CandidateAuthService.java
+++ backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/services/CandidateAuthService.java
@@ -1,8 +1,9 @@
 package com.rcr.core_engine.services;
 
 import com.rcr.core_engine.dtos.*;
-import com.rcr.core_engine.dtos.ApiResponse;
 
 public interface CandidateAuthService {
     ApiResponse registerCandidate(CandidateRegistrationRequest request);
+    void sendOtp(SendOtpRequest request);
+    AuthResponse login(LoginRequest request);
 } 
