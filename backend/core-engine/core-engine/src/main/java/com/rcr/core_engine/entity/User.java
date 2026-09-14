0ac239ef30ccfaaed1db1996e9e0ed0a9998c136
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-09-14 18:07:55 +0000

    feat: add NoArgsConstructor and AllArgsConstructor annotations to User entity

--- backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/entity/User.java
+++ backend/core-engine/core-engine/src/main/java/com/rcr/core_engine/entity/User.java
@@ -19,6 +19,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name = "users")
 @Data
 @Builder
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class User implements UserDetails{
 
     @Id
